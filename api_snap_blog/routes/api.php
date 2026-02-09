393f6d00a742d10511ca923c6c8c0808dabe57e3
Kifk2f <fianyofriedo2@gmail.com>
2026-02-09 23:29:24 +0000

    Création de routes GET et POST exemple

--- api_snap_blog/routes/api.php
+++ api_snap_blog/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 // ---Routes sans paramètres---
 // Route::get('/foo', function () {
@@ -24,4 +25,18 @@ Route::get('/users', function () {
     $user->name = "Kodzovi Friedo FIANYO"; // On ajoute une propriété name à l'objet
     $user->email = "fianyofriedo2@gmail.com"; // On ajoute une propriété email à l'objet
     return response()->json($user); // On retourne l'objet $user sous form de JSON
+});
+
+//Créer les routes avec Get, Post, Put, Patch et Delete
+Route::get('/users', function () {
+    return response()->json(['success' => 'Méthode GET']);
+});
+
+// // Pour que le body de Postman soit pris en compte, il faut ajouter Request $request
+// Route::post('/users', function (Request $request) {
+//     return response()->json(['success' => 'Méthode POST']);
+// });
+
+Route::post('/users', function (Request $request) {
+    return $request->all(); //Récupérer tous les éléments 
 });
